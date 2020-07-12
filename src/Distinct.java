ac99cbbadba40b789f35b6d4bf1a15becff6062e
kuckjwi0928 <skyblue0928@naver.com>
2020-07-12 13:10:43 +0900

    Add distinct

---
+++ src/Distinct.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Distinct {
+  public static void main(String[] args) {
+    System.out.println(Distinct.solution(new int[]{2, 1, 1, 2, 3, 1}));
+  }
+  public static int solution(int[] A) {
+    return Arrays.stream(A).boxed().collect(Collectors.toSet()).size();
+  }
+}
