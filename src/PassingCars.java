79d88be42db4902ccb4c203dec8b5b334bfd511d
kuckjwi0928 <skyblue0928@naver.com>
2020-07-12 13:03:05 +0900

    Add passing cars

---
+++ src/PassingCars.java
@@ -0,0 +1,20 @@
+public class PassingCars {
+  public static void main(String[] args) {
+    System.out.println(PassingCars.solution(new int[]{0, 1, 0, 1, 1}));
+  }
+  public static int solution(int[] A) {
+    int result = 0;
+    int zero = 0;
+    for (int i = 0, len = A.length; i < len; i++) {
+      if (A[i] == 0) {
+        zero++;
+        continue;
+      }
+      result += zero;
+    }
+    if (result < 0 || result > 1000000000) {
+      return -1;
+    }
+    return result;
+  }
+}
