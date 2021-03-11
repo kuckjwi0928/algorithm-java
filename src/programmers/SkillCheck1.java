f8b59f2c2a847bdf40e91b5a4a9e89fe6e227709
kuckjwi0928 <skyblue0928@naver.com>
2021-03-11 15:03:34 +0900

    add programmers skill check

---
+++ src/programmers/SkillCheck1.java
@@ -0,0 +1,29 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SkillCheck1 {
+  public static void main(String[] args) {
+    new SkillCheck1().solution("Zbcdefg");
+  }
+
+  public String solution(String s) {
+    char[] arr = s.toCharArray();
+    Integer[] scores = new Integer[arr.length];
+
+    for (int i = 0; i < arr.length; i++) {
+      scores[i] = (int) arr[i];
+    }
+
+    Arrays.sort(scores, Collections.reverseOrder());
+
+    StringBuilder builder = new StringBuilder();
+
+    for (Integer score : scores) {
+      builder.append((char) score.intValue());
+    }
+
+    return builder.toString();
+  }
+}
