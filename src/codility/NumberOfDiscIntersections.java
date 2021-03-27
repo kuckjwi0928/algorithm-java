118fce5ce64c0614d0287837566a623e959442ac
kuckjwi0928 <skyblue0928@naver.com>
2021-03-27 17:28:34 +0900

    add codility content

---
+++ src/codility/NumberOfDiscIntersections.java
@@ -0,0 +1,41 @@
+package codility;
+
+import java.util.Arrays;
+
+public class NumberOfDiscIntersections {
+  public static void main(String[] args) {
+    solution(new int[]{1, 5, 2, 1, 4, 0});
+  }
+
+  public static int solution(int[] A) {
+    int len = A.length;
+
+    long[] lower = new long[len];
+    long[] upper = new long[len];
+
+    for (int i = 0; i < A.length; i++) {
+      lower[i] = i + A[i] * -1;
+      upper[i] = i + A[i];
+    }
+
+    Arrays.sort(lower);
+    Arrays.sort(upper);
+
+    int intersection = 0;
+    int j = 0;
+
+    for (int i = 0; i < len; i++) {
+      while (j < len && upper[i] >= lower[j]) {
+        intersection += j;
+        intersection -= i;
+        j++;
+      }
+    }
+
+    if (intersection > 10_000_000) {
+      return -1;
+    }
+
+    return intersection;
+  }
+}
---
+++ src/codility/Triangle.java
@@ -0,0 +1,29 @@
+package codility;
+
+import java.util.Arrays;
+
+public class Triangle {
+  public static void main(String[] args) {
+    System.out.println(solution(new int[]{10, 2, 5, 1, 8, 20}));
+  }
+
+  public static int solution(int[] A) {
+    if (A.length < 3) {
+      return 0;
+    }
+
+    Arrays.sort(A);
+
+    for (int i = 0; i < A.length - 2; i++) {
+      long P = A[i];
+      long Q = A[i + 1];
+      long R = A[i + 2];
+
+      if (P + Q > R) {
+        return 1;
+      }
+    }
+
+    return 0;
+  }
+}
