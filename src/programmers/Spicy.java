372b2cc0b220b9293d95d464629210a2145d4ca0
kuckjwi0928 <skyblue0928@naver.com>
2021-06-03 00:26:09 +0900

    add spicy

---
+++ src/programmers/Spicy.java
@@ -0,0 +1,33 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+
+public class Spicy {
+  public static void main(String[] args) {
+    new Spicy().solution(new int[]{1,1,1}, 7);
+  }
+  public int solution(int[] scoville, int K) {
+    PriorityQueue<Integer> queue = Arrays.stream(scoville)
+            .boxed()
+            .collect(Collectors.toCollection(PriorityQueue::new));
+
+    int shuffle = 0;
+
+    while (!queue.isEmpty() && queue.peek() <= K) {
+      if (queue.size() < 2) {
+        return -1;
+      }
+
+      int n1 = queue.poll();
+      int n2 = queue.poll();
+
+      queue.offer(n1 + n2 * 2);
+
+      shuffle++;
+    }
+
+    return shuffle > 0 ? shuffle : -1;
+  }
+}
