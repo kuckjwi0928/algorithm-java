cd217a157823f762e32c2d556838a0e4cf713c98
kuckjwi <rnrghks09@gmail.com>
2021-08-05 15:23:58 +0900

    dd lifeboat

---
+++ src/programmers/Lifeboat.java
@@ -0,0 +1,30 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Lifeboat {
+  public static void main(String[] args) {
+    System.out.println(new Lifeboat().solution(new int[]{70, 50, 80, 50}, 100));
+    System.out.println(new Lifeboat().solution(new int[]{70, 80, 50}, 100));
+  }
+
+  public int solution(int[] people, int limit) {
+    Arrays.sort(people);
+
+    int p1 = 0;
+    int p2 = people.length - 1;
+
+    int count = 0;
+
+    while (p1 <= p2) {
+      int sum = people[p1] + people[p2];
+      if (sum <= limit) {
+        p1++;
+      }
+      p2--;
+      count++;
+    }
+
+    return count;
+  }
+}
