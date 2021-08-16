b200f1a238a9a362bd15472767e6fe29d205b31e
kuckjwi <rnrghks09@gmail.com>
2021-08-17 08:59:43 +0900

    add remove duplicate from sorted array

---
+++ src/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+  public static void main(String[] args) {
+//    new RemoveDuplicatesFromSortedArray().removeDuplicates(new int[]{1,1,2});
+//    new RemoveDuplicatesFromSortedArray().removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4});
+//    new RemoveDuplicatesFromSortedArray().removeDuplicates(new int[]{1});
+    new RemoveDuplicatesFromSortedArray().removeDuplicates(new int[]{1,1});
+//    new RemoveDuplicatesFromSortedArray().removeDuplicates(new int[]{1,2});
+  }
+
+  public int removeDuplicates(int[] nums) {
+    int k = 0;
+    int len = nums.length;
+
+    if (len < 2) {
+      return len;
+    }
+
+    for (int i = 0; i < len - 1; i++) {
+      if (nums[i] != nums[i + 1]) {
+        nums[k++] = nums[i];
+      }
+    }
+
+    if (k != 0 && nums[k - 1] != nums[len - 1]) {
+      nums[k++] = nums[len - 1];
+    }
+
+    return k == 0 ? 1 : k;
+  }
+}
