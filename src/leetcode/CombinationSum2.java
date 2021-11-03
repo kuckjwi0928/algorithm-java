94c73d9cf55412d7619fb2d167cd75ae8c792e01
kuckjwi <rnrghks09@gmail.com>
2021-11-03 12:35:58 +0900

    add combination sum2

--- src/leetcode/CombinationSum2.java
+++ src/leetcode/CombinationSum2.java
@@ -1,18 +1,20 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class CombinationSum {
+public class CombinationSum2 {
 
   public static void main(String[] args) {
-    System.out.println(new CombinationSum().combinationSum(new int[]{2,3,5}, 8));
+    System.out.println(new CombinationSum2().combinationSum2(new int[]{10,1,2,7,6,1,5}, 8));
   }
 
-  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
     List<List<Integer>> result = new ArrayList<>();
+    Arrays.sort(candidates);
     combination(candidates, 0, target, new ArrayList<>(), result);
-    return result;
+    return List.copyOf(result);
   }
 
   private void combination(int[] candidates, int start, int target, List<Integer> cur, List<List<Integer>> result) {
@@ -23,11 +25,11 @@ public class CombinationSum {
 
     for (int i = start; i < candidates.length; i++) {
       int candidate = candidates[i];
-      if (candidate > target) {
+      if (candidate > target || (i != start && candidates[i] == candidates[i - 1])) {
         continue;
       }
       cur.add(candidate);
-      combination(candidates, i, target - candidate, cur, result);
+      combination(candidates, i + 1, target - candidate, cur, result);
       cur.remove(cur.size() - 1);
     }
   }
