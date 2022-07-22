36d01d90933408b3108d77b64331c7cc18dd83b4
kuckjwi <rnrghks09@gmail.com>
2022-07-22 19:54:29 +0900

    add rotate image

---
+++ src/leetcode/RotateImage.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class RotateImage {
+  public static void main(String[] args) {
+    int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+    new RotateImage().rotate(matrix);
+    for (int[] ints : matrix) {
+      System.out.println(Arrays.toString(ints));
+    }
+  }
+  public void rotate(int[][] matrix) {
+    int n = matrix.length;
+    for (int i = 0; i < n / 2; i++) {
+      for (int j = i; j < n - i - 1; j++) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[n - j - 1][i];
+        matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+        matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+        matrix[j][n - i - 1] = temp;
+      }
+    }
+  }
+}
