22704ae6f68e2f6516530434dbe9c51ed0f044b5
kuckjwi <rnrghks09@gmail.com>
2023-04-05 12:27:54 +0900

    점 찍기 풀이

---
+++ src/programmers/exercise/Lessons140107.java
@@ -0,0 +1,15 @@
+package programmers.exercise;
+
+public class Lessons140107 {
+  public static void main(String[] args) {
+    System.out.println(new Lessons140107().solution(2, 4));
+  }
+
+  public long solution(int k, int d) {
+    long answer = 0;
+    for (var i = 0; i <= d; i += k) {
+      answer += Math.sqrt(Math.pow(d, 2) - Math.pow(i, 2)) / k + 1;
+    }
+    return answer;
+  }
+}
